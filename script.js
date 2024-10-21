c6980229d794be32910deda0e470c8d311b2201f
fitszimoon <fitszimoon@gmail.com>
2024-10-22 00:21:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,41 @@ let questions = [
         question: "The engine works in 4 stages; what are these?",
         options: ["Intake, compression, combustion, discharge", "Intake, compression, ignition, exhaust", "Intake, compression, combustion, exhaustion", "Intake, expansion, combustion, expulsion"],
         correctAnswer: 1
-    }
+    },
+    {
+        question: "The engine works in 4 stages; what are these?",
+        options: ["Intake, compression, combustion, discharge", "Intake, compression, ignition, exhaust", "Intake, compression, combustion, exhaustion", "Intake, expansion, combustion, expulsion"],
+        correctAnswer: 1
+    },
+    {
+        question: "The engine works in 4 stages; what are these?",
+        options: ["Intake, compression, combustion, discharge", "Intake, compression, ignition, exhaust", "Intake, compression, combustion, exhaustion", "Intake, expansion, combustion, expulsion"],
+        correctAnswer: 1
+    },
+    {
+        question: "The engine works in 4 stages; what are these?",
+        options: ["Intake, compression, combustion, discharge", "Intake, compression, ignition, exhaust", "Intake, compression, combustion, exhaustion", "Intake, expansion, combustion, expulsion"],
+        correctAnswer: 1
+    },
+    {
+        question: "The engine works in 4 stages; what are these?",
+        options: ["Intake, compression, combustion, discharge", "Intake, compression, ignition, exhaust", "Intake, compression, combustion, exhaustion", "Intake, expansion, combustion, expulsion"],
+        correctAnswer: 1
+    },
+    {
+        question: "The engine works in 4 stages; what are these?",
+        options: ["Intake, compression, combustion, discharge", "Intake, compression, ignition, exhaust", "Intake, compression, combustion, exhaustion", "Intake, expansion, combustion, expulsion"],
+        correctAnswer: 1
+    },
+    {
+        question: "The engine works in 4 stages; what are these?",
+        options: ["Intake, compression, combustion, discharge", "Intake, compression, ignition, exhaust", "Intake, compression, combustion, exhaustion", "Intake, expansion, combustion, expulsion"],
+        correctAnswer: 1
+    },
+    {
+        question: "The engine works in 4 stages; what are these?",
+        options: ["Intake, compression, combustion, discharge", "Intake, compression, ignition, exhaust", "Intake, compression, combustion, exhaustion", "Intake, expansion, combustion, expulsion"],
+        correctAnswer: 1
 ];
 
 let currentQuestionIndex = 0;
